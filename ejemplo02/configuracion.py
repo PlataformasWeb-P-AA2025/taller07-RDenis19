e8f3453333e22491f40ff474bd24c210c26dbe2c
Denis Ruiz <dlruiz7@utpl.edu.ec>
2025-05-20 09:28:01 -0500

    Taller semana 7

---
+++ ejemplo02/configuracion.py
@@ -0,0 +1,13 @@
+# este módulo será usado para posibles configuraciones
+#
+# cadena conector a la base de datos
+#
+# Sqlite
+cadena_base_datos = 'sqlite:///base001.db' 
+
+# Mysql
+# para el uso de este dialecto en SqlAlchemy
+# instalar "pip install PyMySQL"
+# cadena_base_datos = 'mysql+pymysql://usuario:clave@localhost/bd_name'
+
+no hagas nada solo ayudame hacer el archivo de ingreso datos porfavor hazlo bien asi como el ejemplo que te pase solo eso 
