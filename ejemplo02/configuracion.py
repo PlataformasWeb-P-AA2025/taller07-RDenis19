0f60a4ae2567a79b36b226705276698b5130d275
Denis Ruiz <dlruiz7@utpl.edu.ec>
2025-05-20 09:32:56 -0500

    Taller semana 7

--- ejemplo02/configuracion.py
+++ ejemplo02/configuracion.py
@@ -10,4 +10,3 @@ cadena_base_datos = 'sqlite:///base001.db'
 # instalar "pip install PyMySQL"
 # cadena_base_datos = 'mysql+pymysql://usuario:clave@localhost/bd_name'
 
-no hagas nada solo ayudame hacer el archivo de ingreso datos porfavor hazlo bien asi como el ejemplo que te pase solo eso 
--- ejemplo02/ingreso_datos.py
+++ ejemplo02/ingreso_datos.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# se importa la clase(s) del 
+# se importa las clases del 
 # archivo genera_tablas
 from genera_tablas import Club, Jugador
 
 # se importa información del archivo configuracion
 from configuracion import cadena_base_datos
 
-# se genera el enlace al gestor de base de datos
+# se genera enlace al gestor de base de datos
 engine = create_engine(cadena_base_datos)
 
 Session = sessionmaker(bind=engine)
@@ -17,3 +17,12 @@ session = Session()
 # Diccionario para almacenar los objetos Club
 clubs = {}
 
+# Lectura del archivo de datos de clubes
+archivo_clubs = open("data/datos_clubs.txt", "r", encoding="utf-8")
+for linea in archivo_clubs:
+    datos = linea.strip().split(";")
+    nombre = datos[0]
+    deporte = datos[1]
+    fundacion = int(datos[2])
+    
+    
